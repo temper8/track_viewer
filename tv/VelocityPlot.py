2d1de96fb9d4561d23c7e02bcf624634a287d6aa
Alex <temper8@gmail.com>
2024-09-19 03:05:56 +0300

    add animation

--- tv/VelocityPlot.py
+++ tv/VelocityPlot.py
@@ -2,22 +2,28 @@ import tkinter as tk
 import tkinter.ttk as ttk
 
 from matplotlib import cm
+from matplotlib.patches import Circle, Ellipse
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import ( FigureCanvasTkAgg, NavigationToolbar2Tk)
 import numpy as np
 from tv.VerticalNavigationToolbar import VerticalNavigationToolbar2Tk
 from tv.KmlTrack import KmlTrack
 
+def circle(x,y,r):
+    return Ellipse(
+            xy=(x, y),
+            width=r/2,
+            height=r/2,
+            angle=0,
+            facecolor="none",
+            edgecolor="b"
+        )
 
 class VelocityPlot(ttk.Frame):
     def __init__(self, master, track:KmlTrack) -> None:
         super().__init__(master)  
         self.track= track
         self.title = 'Track Plot'
-
-        tb = self.make_toolbar()
-        tb.grid(row=0, column=0, columnspan=2, sticky=tk.N + tk.S + tk.E + tk.W) 
-
 
         self.fig, self.axd = plt.subplot_mosaic([['left', 'right A'], ['left', 'right B'], ['left', 'right C']],
                               figsize=(14, 7), layout="constrained")
@@ -28,61 +34,88 @@ class VelocityPlot(ttk.Frame):
         speed = np.array(track.speed)
         course = np.array(track.course)
         U, V = speed*np.sin(course), speed*np.cos(course)
-        self.axd['left'].scatter(U, V, c='tab:blue',  alpha=0.3, edgecolors='none')
+        self.x_speed = U
+        self.y_speed = V
+        self.axd['left'].scatter(U, V, c='blue',  alpha=0.3, edgecolors='none')
         self.axd['left'].plot(U, V,  alpha=0.3, linewidth=0.5 )
+        self.axd['left'].axis('equal')
         #Q = self.axd['left'].quiver(X, Y, U, V)
 
-        self.axd['right A'].plot(track.alt, label='alt')
+        #self.axd['left'].add_patch(circle(0,0,1.5))
+        self.circ = Circle((0.0, 0.0), 0.5, facecolor="none", edgecolor="b")
+        self.axd['left'].add_artist(self.circ)
+        # now determine nice limits by hand:
+        binwidth = 0.1
+        speed_ymax = np.max(np.abs(speed))
+        lim = (int(speed_ymax/binwidth) + 1) * binwidth
+        self.bins = np.arange(0, lim + binwidth, binwidth)
+        self.axd['right A'].hist(speed, bins=self.bins, label='speed hist')
+        #self.axd['right A'].plot(track.alt, label='alt')
         self.axd['right A'].legend(loc='upper right')
-        self.axd['right B'].plot(track.speed, label='speed')
+
+        self.axd['right B'].hist(speed, bins=self.bins, label='speed hist')
         self.axd['right B'].legend(loc='upper right')
-        self.axd['right C'].plot(track.course, label='course')
+
+        self.axd['right C'].plot(track.speed, label='speed')
+        self.speed_var, = self.axd['right C'].plot(track.speed, label='speed')
         self.axd['right C'].legend(loc='upper right')
 
 
         self.canvas = FigureCanvasTkAgg(self.fig, self)
         self.canvas.draw()
-        self.canvas.get_tk_widget().grid(row=1, column=1, sticky=tk.N + tk.S + tk.E + tk.W)
+        self.canvas.get_tk_widget().grid(row=0, column=1, sticky=tk.N + tk.S + tk.E + tk.W)
         #toobar = NavigationToolbar2Tk(self.canvas, frame)
         tb = VerticalNavigationToolbar2Tk(self.canvas, self)
         tb.update()
-        tb.grid(row=1, column=0, sticky=tk.N)    
-        self.columnconfigure(1, weight=1)
+        tb.grid(row=0, column=0, sticky=tk.N)    
+        self.columnconfigure(0, weight=1)
         #self.rowconfigure(0, weight=1)        
-        self.rowconfigure(1, weight=1)
+        self.rowconfigure(0, weight=1)
+
+        tb = self.make_toolbar()
+        tb.grid(row=1, column=0, columnspan=2, sticky=tk.N + tk.S + tk.E + tk.W) 
+
 
     def make_toolbar(self):
         frame = ttk.Frame(self)
   
         self.chk_var = tk.IntVar(master = self, value=True)
   
-        self.checkbtn = ttk.Checkbutton(master=  frame, text="Log scale", variable=self.chk_var, command=self.checkbtn_changed )
+        #self.checkbtn = ttk.Checkbutton(master=  frame, text="test", variable=self.chk_var, command=self.checkbtn_changed )
         #self.checkbtn.pack(side=tk.LEFT, expand=1, fill=tk.X, padx=5) 
-        ns= len(self.track.whens)
-        self.index_1 = tk.IntVar(master = self, value=0)
-        self.index_1.trace_add('write', self.update_plot)
-        self.slider_1 = tk.Scale(master=  frame, variable = self.index_1, orient = tk.HORIZONTAL, 
+        self.x_speed_var = tk.DoubleVar(master = self, value=0)
+        self.x_speed_var.trace_add('write', self.on_update_value)
+        self.slider_1 = tk.Scale(master=  frame, variable = self.x_speed_var, orient = tk.HORIZONTAL, 
                                     sliderlength = 20,
                                     width = 10,            
-                                    label='index',
-                                    tickinterval= ns/4,
-                                    from_=0, 
-                                    to=ns-1, 
-                                    resolution=1 )
-        #self.slider_1.pack(side=tk.LEFT, expand=1, fill=tk.X, padx=5) 
+                                    label='x speed',
+                                    from_= -2, 
+                                    to= 2, 
+                                    resolution= 0.02 )
+        self.slider_1.pack(side=tk.LEFT, expand=1, fill=tk.X, padx=5) 
 
-        self.index_2 = tk.IntVar(master = self, value=ns)
-        self.index_2.trace_add('write', self.update_plot)
-        self.slider_2 = tk.Scale(master=  frame, variable = self.index_2, orient = tk.HORIZONTAL,
+        self.y_speed_var = tk.DoubleVar(master = self, value=0.0)
+        self.y_speed_var.trace_add('write', self.on_update_value)
+        self.slider_2 = tk.Scale(master=  frame, variable = self.y_speed_var, orient = tk.HORIZONTAL,
                                     sliderlength = 20,
                                     width = 10,            
-                                    label='numbers',
-                                    tickinterval= ns/4,
-                                    from_=1, 
-                                    to=ns, 
-                                    resolution=1 )
-        #self.slider_2.pack(side=tk.LEFT, expand=1, fill=tk.X, padx=5)   
+                                    label='y speed',
+                                    from_=-2, 
+                                    to=2, 
+                                    resolution= 0.02 )
+        self.slider_2.pack(side=tk.LEFT, expand=1, fill=tk.X, padx=5)   
             
+        self.max_speed = tk.DoubleVar(master = self, value=1.1)
+        self.max_speed.trace_add('write', self.on_update_value)
+        self.slider_3 = tk.Scale(master=  frame, variable = self.max_speed, orient = tk.HORIZONTAL,
+                                    sliderlength = 20,
+                                    width = 10,            
+                                    label='max speed',
+                                    from_=0, 
+                                    to=4, 
+                                    resolution= 0.02 )
+        self.slider_3.pack(side=tk.LEFT, expand=1, fill=tk.X, padx=5)   
+
         return frame
         
     def checkbtn_changed(self):
@@ -92,8 +125,24 @@ class VelocityPlot(ttk.Frame):
             self.уscale_log = False
         self.show_series(save_lim = False) 
 
-    def update_plot(self, var, indx, mode):
-        self.show_series()
+    def on_update_value(self, var, indx, mode):
+        x_speed = self.x_speed_var.get()
+        y_speed = self.y_speed_var.get()
+        max_speed = self.max_speed.get()
+        #x, y = speed*np.sin(course), speed*np.cos(course)
+        self.circ.center = x_speed, y_speed
+        self.circ.radius = max_speed
+        speed = np.sqrt(np.square(x_speed)+ np.square(y_speed))
+        course = np.atan2(x_speed, y_speed)
+        print(f"speed= {speed} course= {course} course= {max_speed}")
+        xx_speed = self.x_speed - x_speed
+        yy_speed = self.y_speed - y_speed
+        full_speed = np.sqrt(np.square(xx_speed)+ np.square(yy_speed))
+        self.axd['right B'].clear()
+        self.axd['right B'].hist(full_speed, bins=self.bins, label='speed hist')
+        #self.axd['right C'].plot(full_speed, alpha=0.3, label='speed')
+        self.speed_var.set_ydata(full_speed)
+        self.canvas.draw()
 
     def update(self, series, time_stamp):
         self.fig.suptitle(f'{self.title}. Time={time_stamp}')
