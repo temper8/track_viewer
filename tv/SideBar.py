ae42651c5b1268de663da4cd70241468f900a558
Alex <temper8@gmail.com>
2024-09-21 00:51:57 +0300

    add refresh btn app

--- tv/SideBar.py
+++ tv/SideBar.py
@@ -7,14 +7,14 @@ import tv.WorkSpace as WorkSpace
 
 
 
-def construct(master, command):
+def construct(master, command, btn_command):
     sb = SideBar(master)
     content = WorkSpace.get_file_list()
     ListView(sb, content, command= command).pack(expand=1, fill=tk.BOTH, padx=(10,0), pady=(5,5))
 
     ttk.Separator(sb, orient='horizontal').pack(fill='x')
 
-    ttk.Radiobutton(sb, text="    ",  value="imped", width=30, 
+    ttk.Radiobutton(sb, text="refresh",  value="imped", width=30, command= btn_command,
                     style = 'Toolbutton').pack(expand=0, fill=tk.X)
 
     return sb
